15564ab1247936e054453a50c6bff4f95bbf2fd0
Mohit <msarts1234@gmail.com>
2024-09-23 01:03:36 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
+import "./App.css";
+import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
 const App = () => {
   return (
     <div>
-      <div className="logo">
-        <img src="/images/brand_logo.png" alt="logo" />
-      </div>
-      <ul>
-        <li href="#">Menu</li>
-        <li href="#">Location</li>
-        <li href="#">About</li>
-        <li href="#">Contact</li>
-      </ul>
-      <button>login</button>
+      <Navigation />
+      <Hero />
     </div>
   );
 };
