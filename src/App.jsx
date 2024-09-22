780d3059671bd994d94ef451be7bb7c6ed3e8a0a
Mohit <msarts1234@gmail.com>
2024-09-22 23:19:16 +0530

    these are new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-//import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <div className="App">Hello World</div>
-    </>
+    <div>
+      <div className="logo">
+        <img src="/images/brand_logo.png" alt="logo" />
+      </div>
+      <ul>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+      </ul>
+      <button>login</button>
+    </div>
   );
-}
+};
 
 export default App;
